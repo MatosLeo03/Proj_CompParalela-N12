71994ed1c72ae2eb91ff102be3a5a0607f1ca6b9
Leonardo H. O. Matos <84693356+MatosLeo03@users.noreply.github.com>
2023-03-22 19:45:48 -0300

    Código

--- Lab03/Lab03.c
+++ Lab03/Lab03.c
@@ -1,1 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+
+#define SHM_SIZE  sizeof(int)
+
+int main()
+{
+    int shmid, *data;
+    pid_t pid;
+
+    // área de memória compartilhada
+    if ((shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | 0666)) < 0) {
+        perror("shmget");
+        exit(1);
+    }
+
+    if ((data = shmat(shmid, NULL, 0)) == (int *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+   
+    *data = 5; //Variável compartilhada
+
+    printf("Pai: valor inicial = %d\n", *data);
+
+    if ((pid = fork()) < 0) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {  
+        printf("Valor inicial do filho = %d\n", *data);
+        *data += 2;  
+        printf("Novo valor do filho = %d\n", *data);
+
+        
+        if (shmdt(data) == -1) { //desanexar o segmento de memória compartilhada da área de dados do processo atual.
+            perror("shmdt");
+            exit(1);
+        }
+
+        exit(0);
+    } else {  
+        
+        if (waitpid(pid, NULL, 0) == -1) {
+            perror("waitpid");
+            exit(1);
+        }
+
+        *data *= 4;  
+        printf("Pai: novo valor = %d\n", *data);
+      
+        if (shmdt(data) == -1) {
+            perror("shmdt");
+            exit(1);
+        }
+
+        if (shmctl(shmid, IPC_RMID, NULL) == -1) {
+            perror("shmctl");
+            exit(1);
+        }
+    }
+
+    return 0;
+}
 
