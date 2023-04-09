f2aef70712f5d8130a4fbe07c938397e25bca21c
Leonardo H. O. Matos <84693356+MatosLeo03@users.noreply.github.com>
2023-04-09 16:39:00 -0300

    Rascunho

--- Lab05/Lab05.c
+++ Lab05/Lab05.c
@@ -1,1 +1,15 @@
+void *Pth_mat_vect (void* rank){
+long my_rank = (long) rank;
+int i, j;
+int local_m = m/threads_count;
+int my_first_row = my_rank*local_m;
+int my_last_row = (my_rank+1)*local_m - 1;
 
+for (i = my_first_row; i <= my_last_row; i++)
+	y[i] = 0.0;
+	for (j = 0; j < n; j++)
+		y[i] += A[i][j]*x[j];
+	}
+	
+	return NULL;
+} /*Pth_mat_vect */
