457e5f5aaf0485d655422347a0df25a5fba1747b
Leonardo H. O. Matos <84693356+MatosLeo03@users.noreply.github.com>
2023-04-19 19:36:20 -0300

    Update Lab06.c

--- Lab06/Lab06.c
+++ Lab06/Lab06.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <omp.h>
 
-void Trap (double a, double b, int n, double* global_result_p);
+double f(double x) {
+    return sin(x);
+}
 
-int main (int argc, char* argv[]) {
-	double global_result = 0.0; //store result in global_result
-	double a, b;		    //Left and right endpoints
-	int n;		            //total number of trapezoids
-	int thread_count;
+void Trap(double local_a, double local_b, int local_n, double h, double* my_result_p) {
+    double x, my_result;
+    my_result = (f(local_a) + f(local_b)) / 2.0;
+    for (int i = 1; i <= local_n - 1; i++) {
+        x = local_a + i * h;
+        my_result += f(x);
+    }
+    my_result = my_result * h;
+    *my_result_p = my_result;
+}
 
-	thread_count = strtlol (argv[1], NULL, 10);
-	printf ("Enter a, b, and n\n")
-	scanf ("%lf %lf %d", &a, &b, &n);
-	
-	#pragma omp parallell num_thread (thread_count)
-	trap (a, b, n, &global_result);
+int main() {
+    double global_result = 0.0; 
+    double a, b;                
+    int n;                      
+    int thread_count;
+    
+    printf("A = valor inicial\nB = valor final\nN = número de trapézios
+    printf("Ínserir valores de *a*, *b* e *n* \n");
+    if (scanf("%lf %lf %d", &a, &b, &n) != 3 || n <= 0) {
+        printf("Erro na inserção.\n");
+        return 1;
+    }
 
-	printf ("With n = &d trapezoids, our estimate \n", n);
-	printf ("of the integral from %f to %f = %.14e\n", a, b, global_result);
-	
-	return 0;
-}       // main
+    thread_count = omp_get_max_threads();
+    printf("Utilizando %d threads.\n", thread_count);
 
-void trap (double a, double b, int n, double* global_result_p) {
-	double h, x, my_result;
-	double local_a, local_b;
-	int i, local_n;
-	int my_rank = omp_get_thread_num();
-	int thread_count = omp_get_num_threads();
+    double h = (b - a) / n;
+    #pragma omp parallel num_threads(thread_count)
+    {
+        int my_rank = omp_get_thread_num();
+        int local_n = n / thread_count;
+        double local_a = a + my_rank * local_n * h;
+        double local_b = local_a + local_n * h;
+        double my_result;
+        Trap(local_a, local_b, local_n, h, &my_result);
+        #pragma omp critical
+        global_result += my_result;
+    }
 
-	h = (b-a)/n;
-	local_n = n/thread_count;
-	local_a = a + my_rank*local_n*h;
-	local_b = local_a + local_n*h;
-	my_result = (f(local_a) + f(local_b))/2.0;
-	for (i = 1; i <= local_n - 1; i++) {
-		x = local_a + i*h;
-		my_result += f(x);
-	}
-	my_result = my_result*h;
+    printf("Com n = %d trapézios", n);
+    printf("O valor aproximado da integral entreo %f e %f = %.14e\n", a, b, global_result);
 
-#pragma omp critical
-	*global_result_p += my_result;
-
-} // Trap
-
-
+    return 0;
+}
